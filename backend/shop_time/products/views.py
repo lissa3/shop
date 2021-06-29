dcf461d90fbf43f1c8d6c8d2f6064750a4d4eed2
Tanja Rietveldt <diafox2015@gmail.com>
2021-06-30 00:33:06 +0200

    change filter for product for custom fields: icontains lookup

--- backend/shop_time/products/views.py
+++ backend/shop_time/products/views.py
@@ -1,4 +1,5 @@
 from django.db.models import When, Case, Count, Avg
+from django.db.models.lookups import Lookup
 from rest_framework.response import Response
 from rest_framework import status
 # from rest_framework.views import APIView
@@ -6,20 +7,30 @@ from rest_framework.mixins import RetrieveModelMixin, ListModelMixin
 from rest_framework.viewsets import GenericViewSet
 
 from rest_framework.permissions import AllowAny
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend   # installed app dj_filters
+from django_filters import rest_framework as dj_filters         # installed app dj_filters
+from django_filters import filters as custom_filters            # installed app dj_filters
+from rest_framework import filters                              # for SearchFilter (built-in)
 
 from .models import Product
 from serializers.prods.prod_ser import ProductSerializer
+
+class ProductFilterCustom (dj_filters.FilterSet):
+    name = custom_filters.CharFilter(lookup_expr='icontains')
+    description = custom_filters.CharFilter(lookup_expr='icontains')
+    class Meta:
+        model = Product
+        fields = ('price', 'in_stock','name','categ','description')
+    
 
 class ProductRetrListViewSet(ListModelMixin,RetrieveModelMixin, GenericViewSet):
     """get single prod object and list of products (default ordering ascend price('- price'))"""
     permission_classes = (AllowAny,)
     serializer_class = ProductSerializer
     lookup_field = 'slug'
-
     filter_backends = (filters.SearchFilter,DjangoFilterBackend,)
-    filterset_fields = ['price', 'in_stock','name','categ']
+    # filterset_fields = ['price', 'in_stock','name','categ'] # replaced by custom filterset_class
+    filterset_class = ProductFilterCustom
 
     search_fields = ['name','price','categ__name'] # default ?search=cat (charField,textField)
 
